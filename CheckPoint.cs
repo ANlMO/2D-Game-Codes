3fd954e38e6ad2fef6abe4366ade3f0bd8394766
Animo <68300566+Animo-GD@users.noreply.github.com>
2020-07-29 12:21:12 +0200

    Add files via upload

---
+++ CheckPoint.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckPoint : MonoBehaviour
+{
+    private CheckPointManager GM;
+
+    private void Awake()
+    {
+        GM = GameObject.FindGameObjectWithTag("GM").GetComponent<CheckPointManager>();
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.CompareTag("Black"))
+        {
+            GM.LastCheckPoint = transform.position;
+        }
+    }
+}
